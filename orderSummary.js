c40ca2ee9eeaffe5ee9fc89aab95625e75999668
Kärt Aru <115346219+kartaru@users.noreply.github.com>
2022-11-18 23:29:14 +0200

    Create orderSummary.js

---
+++ orderSummary.js
@@ -0,0 +1,84 @@
+var tellimus = { 
+    "id": 2113, 
+    "orderNumber": "E210126001", 
+    "createdAt": "2021-01-26 16:04:24", 
+    "clientName": "Kalle Tali", 
+    "deliveryType": "Omniva smartpost", 
+    "deliveryValue": 178, 
+    "rows": [ 
+      { 
+        "article_id": 31, 
+        "name": "Kohvimasin De'Longhi Dolce Gusto Piccolo EDG100.W", 
+        "product_code": "257549", 
+        "price": 49.1666666667, 
+        "amount": 1, 
+        "vat": 0.2 
+      }, 
+      { 
+        "article_id": 23420, 
+        "name": "Kohvikapslid Nescafe Dolce Gusto Espresso Intenso, 128 g, 16 tk", 
+        "product_code": "355836", 
+        "price": 5.0666666667, 
+        "amount": 2, 
+        "vat": 0.2 
+      }, 
+      { 
+        "article_id": 23421, 
+        "name": "Kohvikapslid Nescafe Dolce Gusto Latte Macchiato, 194 g, 16 tk", 
+        "product_code": "000000000501267301", 
+        "price": 5.825, 
+        "amount": 1, 
+        "vat": 0.2 
+      } 
+    ] 
+  } 
+  var tellimus = { 
+    "id": 2113, 
+    "orderNumber": "E210126001", 
+    "createdAt": "2021-01-26 16:04:24", 
+    "clientName": "Kalle Tali", 
+    "deliveryType": "Omniva smartpost", 
+    "deliveryValue": 178, 
+    "rows": [ 
+      { 
+        "article_id": 31, 
+        "name": "Kohvimasin De'Longhi Dolce Gusto Piccolo EDG100.W", 
+        "product_code": "257549", 
+        "price": 49.1666666667, 
+        "amount": 1, 
+        "vat": 0.2 
+      }, 
+      { 
+        "article_id": 23420, 
+        "name": "Kohvikapslid Nescafe Dolce Gusto Espresso Intenso, 128 g, 16 tk", 
+        "product_code": "355836", 
+        "price": 5.0666666667, 
+        "amount": 2, 
+        "vat": 0.2 
+      }, 
+      { 
+        "article_id": 23421, 
+        "name": "Kohvikapslid Nescafe Dolce Gusto Latte Macchiato, 194 g, 16 tk", 
+        "product_code": "000000000501267301", 
+        "price": 5.825, 
+        "amount": 1, 
+        "vat": 0.2 
+      } 
+    ] 
+
+
+
+  } 
+  function orderSummary(rows) {
+    let total = 0;
+    for (i=0; i<rows.length; i++) {
+        let row = rows[i]
+        let productPrice = row.price * row.amount;
+        total += productPrice
+        console.log("Toode: "+row.name, "Hind: "+row.price, "Kogus "+row.amount, "Kogus: "+productPrice);
+    }
+    console.log("Kokku: "+total);
+    console.log("+km: "+total * 1.2);
+  }
+
+  orderSummary(tellimus.rows);
